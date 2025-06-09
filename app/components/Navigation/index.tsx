27114fc0f1a19fb045f2f846e5dcb59968d46d95
Christina Lebron <cmlebron14@gmail.com>
2025-06-08 20:57:00 -0400

    added logo image in navigation component

--- app/components/Navigation/index.tsx
+++ app/components/Navigation/index.tsx
@@ -1,9 +1,16 @@
+import Image from "next/image";
 import Navbar from "../Navbar";
 
 const Navigation = () => {
   return (
     <>
       {/* Logo */}
+      <Image
+        src="/images/logo-large.svg"
+        alt="finance logo"
+        width={122}
+        height={22}
+      />
       <Navbar />
       {/* Button */}
     </>
