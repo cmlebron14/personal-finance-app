2c811af943d9d914f9e5cb6ccd1bb0c5ee1542e2
Christina Lebron <cmlebron14@gmail.com>
2025-06-25 23:33:19 -0400

    navigation styling for tablet

--- app/components/navigation/Navigation.tsx
+++ app/components/navigation/Navigation.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState, useEffect } from "react";
 import { useSidebar } from "@/app/context/SidebarContext";
 import Logo from "./Logo";
 import Navbar from "./Navbar";
@@ -7,14 +8,25 @@ import styles from "./navigation.module.css";
 
 const Navigation = () => {
   const { isOpen } = useSidebar();
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  const handleResize = () => {
+    setIsDesktop(window.innerWidth > 768);
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <div
       className={`${styles.navigation} ${isOpen ? styles.open : styles.closed}`}
     >
-      <Logo />
+      {isDesktop && <Logo />}
       <Navbar />
-      <ToggleButton />
+      {isDesktop && <ToggleButton />}
     </div>
   );
 };
